6c1370ae03d5201ce7fc37e71e7b2fe9048a4942
Ivan <57369435+ratioglobus@users.noreply.github.com>
2024-03-18 11:22:20 +0300

    Update userInfoValidate.js

--- middlewares/userInfoValidate.js
+++ middlewares/userInfoValidate.js
@@ -2,7 +2,7 @@ import { Joi, celebrate } from 'celebrate';
 
 export default celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
   }),
 });
